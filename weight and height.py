b4685193b5f5c2acd88c2a432c5180f90edf0044
Sharmika13 <136619979+Sharmika13@users.noreply.github.com>
2023-06-22 21:44:47 +0530

    Add files via upload

---
+++ weight and height.py
@@ -0,0 +1,14 @@
+Python 3.11.4 (tags/v3.11.4:d2340ef, Jun  7 2023, 05:45:37) [MSC v.1934 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+= RESTART: C:\Users\Niroja Gananathan\AppData\Local\Programs\Python\Python311\myfirstprogram.py
+Hello, World!
+What is your name? sharmika
+Hello  sharmika
+>>> 
+>>> weight=44
+>>> print(weight)
+44
+>>> height=90
+>>> print(height)
+90
